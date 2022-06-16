4ede0e77d0aa68c35dc45c4842331bc6b82f518c
Cathy Dao <cathydao95@gmail.com>
2022-06-15 21:22:49 -0700

    add useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Time from "./components/Time";
+// import Buttons from "./components/Buttons";
+import Start from "./components/Start";
+import Counter from "./components/Counter";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [running, setRunning] = useState(false);
+  const [timer, setTimer] = useState({
+    hours: "",
+    minutes: "",
+    seconds: "",
+  });
+
+  function handleStart() {
+    setRunning(true);
+    console.log(timer);
+  }
+
+  function pauseCount() {
+    console.log("pausing");
+  }
+
+  function restartCount() {
+    console.log("restarting");
+    setRunning(false);
+  }
+
+  // console.log(timer);
+
+  function handleChange(event) {
+    setTimer((prevTimer) => {
+      return {
+        ...prevTimer,
+        [event.target.name]: Number(event.target.value),
+      };
+    });
+    countDown();
+  }
+
+  function countDown() {
+    console.log("this works");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Countdown Timer</h1>
+      {running ? (
+        <div>
+          <Counter
+            pauseCount={pauseCount}
+            restartCount={restartCount}
+            timer={timer}
+          />
+        </div>
+      ) : (
+        <div>
+          <Time placeholder="HH" name="hours" handleChange={handleChange} />
+          <Time placeholder="MM" name="minutes" handleChange={handleChange} />
+          <Time placeholder="SS" name="seconds" handleChange={handleChange} />
+          <Start handleStart={handleStart} />
+        </div>
+      )}
     </div>
   );
 }
